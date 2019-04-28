15b0d6126bebfbdde2541367531e9ec745a722a0
Breno Costa <breno.costa182@gmail.com>
2019-04-27 21:46:05 -0300

    Adicionando logica para 2 processos

--- main.c
+++ main.c
@@ -4,32 +4,39 @@
 #include"Processo.h"
 #include"InicializadorDeProcessos.h"
 
-int tempoDeExecucaoDoGerenciador;
-
+int tempoDeExecucaoDoGerenciador = 0;
+int i;
 
 void main()
 {
-  Processo processo;
-  processo = incializaProcesso(processo);
+  Processo processo[2];
+  for(i=0; i<2; i++){
+    processo[i] = incializaProcesso(processo[i]);
 
-  printf("Dados do processo de PID : %d \n",processo.pid);
-  printf("Status da execucao do processo :%s\n",processo.status);
-  printf("Tempo de execucao do processo : %d\n",processo.tempoDeExecucao );
-  puts("\n\n\n");
+    printf("Dados do processo de PID : %d \n",processo[i].pid);
+    printf("Status da execucao do processo :%s\n",processo[i].status);
+    printf("Tempo de execucao do processo : %d\n",processo[i].tempoDeExecucao );
+    puts("\n\n\n");
+  }
 
-
-  tempoDeExecucaoDoGerenciador = processo.tempoDeExecucao + 1;
+  for(i=0; i<2; i++){
+    tempoDeExecucaoDoGerenciador = tempoDeExecucaoDoGerenciador+ processo[i].tempoDeExecucao;
+  }
 
   while (tempoDeExecucaoDoGerenciador > 0) {
-      if (processo.tempoDeExecucao == 0) {
-        printf("Processo de PID : %d finalizado \n", processo.pid );
-        break;
-      }
-      printf(" Processo de PID: %d executando \n", processo.pid);
-      printf("Tempo restante de execucao: %d \n", processo.tempoDeExecucao);
-      printf("Status do processo : %s\n", processo.status);
+      for(i=0;i<2;i++){
+        while (processo[i].tempoDeExecucao > 0 ) {
+          if ((processo[i].tempoDeExecucao - 1) == 0) {
+          printf("Processo de PID : %d finalizado \n\n\n\n", processo[i].pid );
+          break;
+          }
+          printf(" Processo de PID: %d executando \n", processo[i].pid);
+          printf("Tempo restante de execucao: %d \n", processo[i].tempoDeExecucao);
+          printf("Status do processo : %s\n", processo[i].status);
 
-      processo.tempoDeExecucao--;
-      tempoDeExecucaoDoGerenciador--;
+          processo[i].tempoDeExecucao--;
+        }
+        tempoDeExecucaoDoGerenciador--;
+    }
   }
 }
